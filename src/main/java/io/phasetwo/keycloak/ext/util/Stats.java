263637a2d35c115fab83877208229a6359fd2266
Garth <244253+xgp@users.noreply.github.com>
2023-09-08 22:04:30 +0200

    changed stats to get

--- src/main/java/io/phasetwo/keycloak/ext/util/Stats.java
+++ src/main/java/io/phasetwo/keycloak/ext/util/Stats.java
@@ -1,7 +1,9 @@
 package io.phasetwo.keycloak.ext.util;
 
-
-import java.util.HashMap;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.UriBuilder;
+import java.util.List;
 import java.util.Map;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -11,26 +13,31 @@ import org.keycloak.broker.provider.util.SimpleHttp;
 public class Stats {
 
   public static final String PHASETWO_ANALYTICS_DISABLED_KEY = "PHASETWO_ANALYTICS_DISABLED";
+  // todo make a redirect
   public static final String PHASETWO_ANALYTICS_URL = "https://eooemvvcxy5k16a.m.pipedream.net";
 
   public static void collect(String name, String version, String commit, Object... args) {
-    Map<String, Object> info = new HashMap<String, Object>();
-    info.put("name", name);
-    info.put("version", version);
-    info.put("commit", commit);
-    if (args != null) info.put("args", args);
+    MultivaluedMap<String, Object> info = new MultivaluedHashMap<String, Object>();
+    info.add("name", name);
+    info.add("version", version);
+    info.add("commit", commit);
+    if (args != null) info.addAll("args", args);
     collect(info);
   }
 
-  public static void collect(Map<String, Object> info) {
+  public static void collect(MultivaluedMap<String, Object> info) {
     String enabled = System.getenv(PHASETWO_ANALYTICS_DISABLED_KEY);
     if (Boolean.valueOf(enabled)) {
       return; // do nothing if anaylitics is disabled
     }
 
+    UriBuilder u = UriBuilder.fromUri(PHASETWO_ANALYTICS_URL);
+    for (Map.Entry<String, List<Object>> e : info.entrySet()) {
+      u.queryParam(e.getKey(), e.getValue().toArray());
+    }
+
     try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-      SimpleHttp.Response response =
-          SimpleHttp.doPost(PHASETWO_ANALYTICS_URL, httpClient).json(info).asResponse();
+      SimpleHttp.doPost(u.build().toString(), httpClient).json(info).asResponse();
     } catch (Exception ignore) {
     }
   }
